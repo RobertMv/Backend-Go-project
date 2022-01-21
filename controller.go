6040811e01bf199c9eec24a2f7c16500d158e50d
RobertMv <robert.mulyukov@gmail.com>
2022-01-21 22:24:54 +0300

    created router for three paths

---
+++ controller.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/restaurants", restaurantHandler)
+	router.HandleFunc("/employees", employeeHandler)
+	router.HandleFunc("/positions", positionHandler)
+	http.Handle("/", router)
+
+	err := http.ListenAndServe(":2222", nil)
+	if err != nil {
+		return
+	}
+}
+
+func positionHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Position page")
+	if err != nil {
+		return
+	}
+}
+
+func restaurantHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Restaurant Page")
+	if err != nil {
+		return
+	}
+}
+
+func employeeHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Employee Page")
+	if err != nil {
+		return
+	}
+}
